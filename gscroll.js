73b7b150d7dc5c03acdf04bf48a090e7a342a8dd
Adam Pearce <adam.r.pearce@gmail.com>
2014-12-30 23:22:53 -0500

    keyboard

--- gscroll.js
+++ gscroll.js
@@ -3,18 +3,20 @@ function gscroll() {
       windowHeight,
       dispatch = d3.dispatch("scroll", "active"),
       i = NaN,
+      startPos = 0,
       sectionPos = []
 
-  var sections = d3.selectAll("section")
+  sections = d3.selectAll("section")
   var n = sections.size()
 
   d3.select(window)
       .on("scroll.stack", reposition)
       .on('scroll.resize', resize)
+      .on('keydown.resize', keydown)
 
 
   function reposition(){
-    var i1 = d3.bisect(sectionPos, pageYOffset + 0)
+    var i1 = d3.bisect(sectionPos, pageYOffset - 10)
     i1 = Math.min(n - 1, i1)
     if (i != i1){
       sections.classed('active', function(d, i){ return i === i1 })
@@ -27,8 +29,9 @@ function gscroll() {
 
   function resize(){
     sectionPos = []
-    sections.each(function(){
-      sectionPos.push(this.getBoundingClientRect().top) })
+    sections.each(function(d, i){
+      if (!i) startPos = this.getBoundingClientRect().top
+      sectionPos.push(this.getBoundingClientRect().top - startPos) })
   }
 
 
@@ -37,6 +40,42 @@ function gscroll() {
     reposition();
     return true;
   });
+
+
+  function keydown() {
+    var delta;
+    switch (d3.event.keyCode) {
+      case 39: // right arrow
+      if (d3.event.metaKey) return;
+      case 40: // down arrow
+      case 34: // page down
+      delta = d3.event.metaKey ? Infinity : 1; break;
+      case 37: // left arrow
+      if (d3.event.metaKey) return;
+      case 38: // up arrow
+      case 33: // page up
+      delta = d3.event.metaKey ? -Infinity : -1; break;
+      case 32: // space
+      delta = d3.event.shiftKey ? -1 : 1;
+      break;
+      default: return;
+    }
+
+    var i1 = Math.max(0, Math.min(i + delta, n - 1))
+    d3.select(document.documentElement)
+        .interrupt()
+      .transition()
+        .duration(500)
+        .tween("scroll", function() {
+          var i = d3.interpolateNumber(pageYOffset, sectionPos[i1]);
+          return function(t) { scrollTo(0, i(t)); };
+        })
+
+    d3.event.preventDefault();
+  }
+
+
+
 
   d3.rebind(gscroll, dispatch, "on");
 
