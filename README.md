d2e6ced19c4baf2eec71ad80fb8dca3e4c882295
Adam Pearce <1wheel@gmail.com>
2015-02-04 11:58:18 -0500

    starts documentation

---
+++ README.md
@@ -0,0 +1,7 @@
+# graph-scroll.js
+
+Simple scrolling events for [d3](https://github.com/mbostock/d3) graphs. Based on [stack](https://github.com/mbostock/stack.git)
+
+
+
+
--- graph-scroll.js
+++ graph-scroll.js
@@ -1,32 +1,14 @@
-function gscroll() {
+function graphScroll() {
   var windowHeight,
       dispatch = d3.dispatch("scroll", "active"),
       sections = d3.select('null'),
       i = NaN,
       sectionPos = [],
-      n;
+      n
       fixed = d3.select('null'),
       isFixed = null,
       container = d3.select('body'),
-      containerStart = 0;
-
-  //start emiting events on
-  function rv(els){
-    sections = els
-    n = sections.size()
-
-    d3.select(window)
-        .on("scroll.gscroll", reposition)
-        .on('resize.gscroll', resize)
-        .on('keydown.gscroll', keydown)
-  
-    resize()
-    d3.timer(function() {
-      reposition();
-      return true;
-    });
-  }
-
+      containerStart = 0
 
   function reposition(){
     var i1 = d3.bisect(sectionPos, pageYOffset - 10 - containerStart)
@@ -58,22 +40,22 @@ function gscroll() {
   }
 
   function keydown() {
-    var delta;
+    var delta
     switch (d3.event.keyCode) {
       case 39: // right arrow
-      if (d3.event.metaKey) return;
+      if (d3.event.metaKey) return
       case 40: // down arrow
       case 34: // page down
-      delta = d3.event.metaKey ? Infinity : 1; break;
+      delta = d3.event.metaKey ? Infinity : 1 ;break
       case 37: // left arrow
-      if (d3.event.metaKey) return;
+      if (d3.event.metaKey) return
       case 38: // up arrow
       case 33: // page up
-      delta = d3.event.metaKey ? -Infinity : -1; break;
+      delta = d3.event.metaKey ? -Infinity : -1 ;break
       case 32: // space
-      delta = d3.event.shiftKey ? -1 : 1;
-      break;
-      default: return;
+      delta = d3.event.shiftKey ? -1 : 1
+      ;break
+      default: return
     }
 
     var i1 = Math.max(0, Math.min(i + delta, n - 1))
@@ -82,13 +64,15 @@ function gscroll() {
       .transition()
         .duration(500)
         .tween("scroll", function() {
-          var i = d3.interpolateNumber(pageYOffset, sectionPos[i1] + containerStart);
-          return function(t) { scrollTo(0, i(t)); };
+          var i = d3.interpolateNumber(pageYOffset, sectionPos[i1] + containerStart)
+          return function(t) { scrollTo(0, i(t)) }
         })
 
-    d3.event.preventDefault();
+    d3.event.preventDefault()
   }
 
+
+  var rv ={}
 
   rv.container = function(_x){
     if (!_x) return container
@@ -104,7 +88,27 @@ function gscroll() {
     return rv
   }
 
-  d3.rebind(rv, dispatch, "on");
+  rv.sections = function (_x){
+    if (!_x) return sections
 
-  return rv;
+    sections = _x
+    n = sections.size()
+
+    d3.select(window)
+        .on("scroll.gscroll", reposition)
+        .on('resize.gscroll', resize)
+        .on('keydown.gscroll', keydown)
+    
+    resize()
+    d3.timer(function() {
+      reposition()
+      return true
+    })
+
+    return rv
+  }
+
+  d3.rebind(rv, dispatch, "on")
+
+  return rv
 }
